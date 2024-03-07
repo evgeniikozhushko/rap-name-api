26680804553c9914a68756218709c290325781be
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2024-03-06 21:54:39 -0700

    Added an image of Ryan coocking

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+// Serve static files from the public directory
+app.use(express.static('public'))
+
 const rappers = {
     '21 savage': {
         'age': 21,
